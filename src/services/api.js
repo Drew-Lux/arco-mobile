c483268e82ea36f57be432ae0432ed2f9a1119bf
Drew-Lux <d.modiselle@outlook.com>
2026-07-20 15:38:14 +0200

    Added Token functions for when user is logged in and when user logs out.

--- src/services/api.js
+++ src/services/api.js
@@ -13,4 +13,36 @@ const apiClient = axios.create({
   },
 });
 
-// The Request Interceptor: Ru
+// The Request Interceptor: Runs automatically before every fetch
+apiClient.interceptors.reqest.use(
+  async (config) => {
+    const token = await Vault.getToken();
+
+    if (token) {
+      // If a token exists in the hardware vault, attach it to the header
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// The Response Interceptor: Catches global errors (like expired tokens)
+apiClient.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    // If the backend says our token is dead (401 Unauthorized)
+    if (error.response && error.response.status === 401) {
+      console.log("[Arco API] Token expired or invalid. Evicting user...");
+      await Vault.destroyToken();
+      // In a full production app, would also trigger a Zustand state
+      // update here to force the UI back to the Auth screen.
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default apiClient;
